52dc946313772386bb3c6391755a452f932a7885
Alex STANCIU <astanciu@e1p63.cluj.42.fr>
2015-12-08 17:57:12 +0100

    tests for libft

---
+++ test.sh
@@ -0,0 +1,10 @@
+cd
+rm -rf teste
+git clone https://github.com/yyang42/moulitest.git teste
+cd teste
+touch config.ini
+echo "################################################################################
+# Use absolute path
+################################################################################
+
+LIBFT_PATH = ~/ACADEMY-PLUS/libft" > config.ini
