bde9bb9d466a3d22e0babc154d934801c939f1be
Alex STANCIU <astanciu@e1p63.cluj.42.fr>
2015-12-08 19:07:35 +0100

    fixed

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: astanciu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 15:58:59 by astanciu          #+#    #+#             */
-/*   Updated: 2015/12/08 16:00:36 by astanciu         ###   ########.fr       */
+/*   Updated: 2015/12/08 19:06:40 by astanciu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,17 @@ int		ft_atoi(const char *str)
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '+')
+	{
 		i++;
+		if (!ft_isdigit(str[i]))
+			return (0);
+	}
 	if (str[i] == '-')
 	{
 		is_negative = 1;
 		i++;
+		if (!ft_isdigit(str[i]))
+			return (0);
 	}
 	while ((str[i] <= 57 && str[i] >= 48))
 		conv = (conv * 10) + (str[i++] - 48);
