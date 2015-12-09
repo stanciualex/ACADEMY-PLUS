ad5fc5f87fd45897c37b8fb9e75e94df69308046
Alex STANCIU <astanciu@e1p4.cluj.42.fr>
2015-12-09 16:13:00 +0100

    fixed

--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: astanciu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 16:33:18 by astanciu          #+#    #+#             */
-/*   Updated: 2015/12/09 16:10:08 by astanciu         ###   ########.fr       */
+/*   Updated: 2015/12/09 16:12:39 by astanciu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	char			*a;
 	unsigned int	i;
 	unsigned int	j;
-	
+
 	if (s == NULL)
 		return (NULL);
 	a = (char *)malloc(sizeof(char) * (len + 1));
